4a0b4808142de241956a7ecfc3d2155f26328626
Gokulnath Anand <41362808+Gokulnath99@users.noreply.github.com>
2023-01-01 22:15:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      scrollBehavior: {
+        'smooth': 'smooth',
+        'auto': 'auto',
+        'instant': 'instant',
+      },
+      animation: {
+        'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
+      }
+    }
+  },
+  variants: {
+    scrollBehavior: ['responsive'],
+  },
+  plugins: [],
+}
