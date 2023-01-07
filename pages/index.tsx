c06f390619bb42ef5ca52adf70d9a603832b6060
Gokulnath99 <gokulnathanand23@gmail.com>
2023-01-07 16:17:17 +0530

    created skill and experience section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,14 @@ import { Inter } from "@next/font/google";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Experience from "../components/Experience";
-import Skills from "../components/Skills";
+import SkillExp from "../components/SkillExp";
 import Projects from "../components/Projects";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen md:snap-y md:snap-mandatory overflow-x-hidden overflow-y-scroll z-0">
       <Head>
         <title>Gokul's Portfolio</title>
       </Head>
@@ -30,14 +29,14 @@ export default function Home() {
       </section>
 
       {/* Experience */}
-      <section id="experience" className="snap-center">
-        <Experience />
+      <section id="SkillExp" className="snap-center">
+        <SkillExp />
       </section>
 
       {/* skills */}
-      <section id="skills" className="snap-center">
+      {/* <section id="skills" className="snap-center">
         <Skills />
-      </section>
+      </section> */}
 
       {/* project */}
       <section id="project" className="snap-center">
